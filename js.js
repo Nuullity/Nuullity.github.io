e66032ab2cf52e5fe0ab7ddd8a6eefb7bcaa0e5d
Nuullity <107263669+Nuullity@users.noreply.github.com>
2022-08-01 12:56:07 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,14 @@
+
+
+  for (let cont = 10; cont < 11; cont--) {
+ if ( cont < 1)
+{ 
+   alert(`Denovo Otario`);
+   cont = 10 
+
+}
+
+
+    // Runs 5 times, with values of step 0 through 4.
+    alert(`Contador de clicks ${cont}`);
+  }
